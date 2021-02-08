5c4723b62b27c107a2573a78373577f7bbed03a7
xZero707 <xzero@elite7hackers.net>
2021-02-08 10:29:09 +0100

    Add argument validation
    Retroactive commit
    
    - Revision: f3e905e2f6c4b9496904c8251b51fbab99c600df

--- src/attr.sh
+++ src/attr.sh
@@ -1,8 +1,32 @@
 #!/usr/bin/env sh
+# This script is recursive by default
 
-OWNERSHIP=${1:-}
-CHMOD=${2:-}
-TARGET=${3:-}
+# Arguments
+TARGET_PATH=${1:-}
+OWNERSHIP=${2:-}
+FMODE=${3:-}
+DMODE=${4:-}
 
-chown -hR "${OWNERSHIP}" "${TARGET}"
-chmod -R "${CHMOD}" "${TARGET}"
+if [ -z "${TARGET_PATH}" ]; then
+  echo "InvalidArgumentError: Missing target path"
+  exit 1
+fi
+
+if [ -z "${OWNERSHIP}" ]; then
+  echo "InvalidArgumentError: Missing ownership"
+  exit 1
+fi
+
+if [ -z "${FMODE}" ]; then
+  echo "InvalidArgumentError: Missing target path"
+  exit 1
+fi
+
+if [ -z "${DMODE}" ]; then
+  echo "InvalidArgumentError: Missing target path"
+  exit 1
+fi
+
+chown -hR "${OWNERSHIP}" "${TARGET_PATH}"
+chmod -R "${FMODE}" "${TARGET_PATH}"
+find "${TARGET_PATH}" -type d -exec chmod "${DMODE}" {} \;
